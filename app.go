9f2b0710cbd7b63207b23bf0e281a2973a80fa60
Jahir <deislhersanchezfunez.com>
2025-04-20 11:14:45 -0600

    change

--- app.go
+++ app.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"os"
 )
 
 // App struct
@@ -18,28 +15,4 @@ func NewApp() *App {
 
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
-}
-
-func (a *App) CreateMatrix(size int) [][]int {
-	matrix := make([][]int, size)
-	for i := range matrix {
-		matrix[i] = make([]int, size)
-	}
-	fmt.Printf("Matriz creada: %dx%d\n", size, size)
-	return matrix
-}
-
-func (a *App) MazeExist() bool {
-	fileContent, err := os.ReadFile("Mazes.json")
-	if err != nil {
-		return false
-	}
-	var data struct {
-		Mazes []Maze `json:"Mazes"`
-	}
-
-	if err := json.Unmarshal(fileContent, &data); err != nil {
-		return false
-	}
-	return len(data.Mazes) > 0
 }
